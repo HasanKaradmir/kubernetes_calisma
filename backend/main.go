58a6cbdf5bb30162671d05081e54086fb3ba0cad
HasanKaradmir <hasan.karademir345@gmail.com>
2023-03-30 00:47:05 +0300

    Frontend ve Backend icin Dockerfile eklendi.

--- backend/main.go
+++ backend/main.go
@@ -18,13 +18,16 @@ type response struct {
 
 func main() {
 	fmt.Println("Port listening on...")
-	http.HandleFunc("/backend", func(w http.ResponseWriter, r *http.Request) {
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/backend", corsMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// check if the request method is POST
 		if r.Method != "POST" {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
-
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
 		// decode the request body into a loginData struct
 		var data loginData
 		err := json.NewDecoder(r.Body).Decode(&data)
@@ -33,12 +36,30 @@ func main() {
 			return
 		}
 
+		log.Printf("Request body: %+v", data)
+
 		// construct the response object
 		resp := response{Message: "Welcome, " + data.Name + " " + data.LastName + "!"}
 		// encode the response as JSON and send it back to the client
 		json.NewEncoder(w).Encode(resp)
-	})
+		log.Printf("Response: %+v", resp)
+	})))
 
-	// start the server on localhost:5500
-	log.Fatal(http.ListenAndServe("127.0.0.1:80", nil))
+	// start the server on localhost:80
+	log.Fatal(http.ListenAndServe("0.0.0.0:444", mux))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
