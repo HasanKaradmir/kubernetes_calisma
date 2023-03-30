05f6e6003a2ff68927a1968c36d95401ee34e630
HasanKaradmir <hasan.karademir345@gmail.com>
2023-03-31 01:24:51 +0300

    Dockerfile'lar image boyutu kucultulerek yeniden derlendi ve frontend'e yeni ozellikler eklendi.

--- backend/main.go
+++ backend/main.go
@@ -51,9 +51,10 @@ func main() {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
